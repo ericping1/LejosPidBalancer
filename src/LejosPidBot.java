0ac9e695ebfff675036f7d2364e6a2be182cdbe2
ericping1 <eric.ping09@gmail.com>
2017-09-02 22:52:32 -0700

    Set up LejosPidBot and created Scooter thread object

--- src/LejosPidBot.java
+++ src/LejosPidBot.java
@@ -1,13 +1,21 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.Font;
+import lejos.hardware.lcd.GraphicsLCD;
+
 /**
  * LejosPidBot is the main application for the self balancing lego robot.
  */
 public class LejosPidBot {
 
     /**
-     * Main method of program
+     * Main method of program that creates an scooter object which drive around
      * @param args Command line arguments (not used)
      */
     public static void main(String[] args) {
-        System.out.print("Testing intellij commits.");
+        GraphicsLCD lcd = BrickFinder.getDefault().getGraphicsLCD();
+        int screenWidth = lcd.getWidth();
+        int screenHeight = lcd.getHeight();
+        lcd.setFont(Font.getDefaultFont());
+
     }
 }
