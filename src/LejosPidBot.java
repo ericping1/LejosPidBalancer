8f252e4e6e1e10bc84b464ac74cfe5853a3e3b8b
ericping1 <eric.ping09@gmail.com>
2017-09-02 22:19:28 -0700

    First commit into repository

---
+++ src/LejosPidBot.java
@@ -0,0 +1,13 @@
+/**
+ * LejosPidBot is the main application for the self balancing lego robot.
+ */
+public class LejosPidBot {
+
+    /**
+     * Main method of program
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+
+    }
+}
